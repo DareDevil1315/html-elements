464d5f76622df2cfb99668821ded388b62a75266
DareDevil1315 <kusharyan.pradeep@globalschoolsalumni.org>
2026-01-24 12:27:37 +0900

    Add files via upload

---
+++ print.py
@@ -0,0 +1,18 @@
+print("Welcome to Python")
+print(4)
+print("Heloo Python/n")
+print("Hello,5")
+print("Welcome To ",end="*")
+
+x=5
+y="kush"
+print(x)
+print(y)
+codingal="codingal"
+print(codingal)
+name=input("Enter Your Name:")
+print("/nHello", name ,"/nwelocme to codingal")
+
+import keyword
+print("Python Keywords are.../n")
+print(keyword.kwlist)
