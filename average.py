1f670b6712a53e857c657f2714a8229e30b372d5
DareDevil1315 <kusharyan.pradeep@globalschoolsalumni.org>
2026-02-22 13:12:13 +0900

    Add files via upload

---
+++ average.py
@@ -0,0 +1,28 @@
+tree1=98
+tree2=94
+tree3=41
+tree4=95
+tree5=11
+sum=tree1+tree2+tree3+tree4+tree5
+print("the sum of all trees is:", sum)
+average=sum/5
+print("the average of all trees is:" ,average)
+
+amount=int(input("Please Enetr Amount for widthdraw:"))
+note_1=amount/100
+note_3=((Amount%100)%50)//10
+note_2=(Amount%100)//50
+print("note of 100 rupee" ,note_1)
+print("note of 50 rupee" ,note_2)
+print("note of 10 rupee" ,note_3)
+
+print("Enetr marks Obatined in 4 Subjects: ")
+math=int(input("math:"))
+english=int(input("english:"))
+science=int(input("science:"))
+hindi=int(input("hindi:"))
+sum=math+english+science+hindi
+print("sum of math,english,science and hindi=",sum)
+perc=(sum/400)*100
+print(end="Percentage Mark =")
+print(perc)
